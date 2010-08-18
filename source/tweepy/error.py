e8dceb72b8d3ae5cb043bb5473641fef734ede8b
chirshan <codingai@gmail.com>
2010-08-19 07:32:52 +0800

    first commit

---
+++ source/tweepy/error.py
@@ -0,0 +1,13 @@
+# Tweepy
+# Copyright 2009-2010 Joshua Roesslein
+# See LICENSE for details.
+
+class TweepError(Exception):
+    """Tweepy exception"""
+
+    def __init__(self, reason):
+        self.reason = str(reason)
+
+    def __str__(self):
+        return self.reason
+
