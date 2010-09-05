56366928f3fb55aa081dcaa973f1f09cce0eb695
chirshan <codingai@gmail.com>
2010-09-05 11:33:16 +0800

    Update to OAuth

--- source/app.py
+++ source/app.py
@@ -19,62 +19,67 @@ class TwitterDB(db.Model):
 
 # ---- The Job Handler --------
 class TwitterBot(webapp.RequestHandler):
-    def get(self):
-        twitter_id = "yourtwitterid" # Configuraton: Add your twitter username
-        twitter_pwd = "yourtwitterpwd"   # Configuration: Add your twitter password
-        auth = tweepy.BasicAuthHandler(twitter_id, twitter_pwd)
-        bot = tweepy.API(auth)
-        shortapi = bitly.Api(login='yourbitlyid', apikey='yourbitlykey')
-
-        feed = 'http://www.reddit.com/r/programming/.rss'
-        atomxml = feedparser.parse(feed)
-        entries = atomxml['entries']
-
-        tweets = ''
-        if len(entries) != 0:
-            entries.reverse()
-            for x in range(len(entries)):
-                entry = entries[x]
-                title = str(unicode(entry['title']).encode("utf-8"))
-                link = str(unicode(entry['link']).encode("utf-8"))
-                myid = str(unicode(entry['id']).encode("utf-8"))
-                summary = str(unicode(entry['summary']).encode("utf-8"))
-
-                num_comments = 0
-                status = ''
-                soup = BeautifulSoup(summary)
-                links = soup.findAll('a')
-                for link in links:
-                    if link['href'].find('/user/') < 0:
-                        status += ' ' + shortapi.shorten(link['href'])
-                    if link.string.find('comments') > 0:
-                        status += ' ' + link.string
-                        num_comments = int(link.string[1:-1].split()[0])
-                status = title[:(140 - len(status))] + status
+	def get(self):
+		consumer_key = ""
+		consumer_secret = ""
+		access_token = ""
+		access_token_secret = ""
+		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_token_secret)
+		bot = tweepy.API(auth)
+		shortapi = bitly.Api(login='', apikey='')
+		feed = 'http://www.reddit.com/r/programming/.rss'
+		atomxml = feedparser.parse(feed)
+		entries = atomxml['entries']
+		tweets = ''
+		
+		if len(entries) != 0:
+			entries.reverse()
+			for x in range(len(entries)):
+				entry = entries[x]
+				title = str(unicode(entry['title']).encode("utf-8"))
+				link = str(unicode(entry['link']).encode("utf-8"))
+				myid = str(unicode(entry['id']).encode("utf-8"))
+				summary = str(unicode(entry['summary']).encode("utf-8"))
+				num_comments = 0
+				status = ''
+				soup = BeautifulSoup(summary)
+				links = soup.findAll('a')
+				for link in links:
+					if link['href'].find('/user/') < 0:
+						status += ' ' + shortapi.shorten(link['href'])
+					if link.string.find('comments') > 0:
+						status += ' ' + link.string
+ 						num_comments = int(link.string[1:-1].split()[0])
+				
+				try:
+					status = title[:(140 - len(status))] + status
+				except:
+					status = repr(title[:(140 - len(status))]) + status
                 
-                query = TwitterDB.all()
-                query.filter('reddit_id =', myid)
-                res = query.fetch(1)
+				query = TwitterDB.all()
+				query.filter('reddit_id =', myid)
+				res = query.fetch(1)
                 
-                if len(res) == 0 and num_comments > 5:
-                    tweets += '<p>' + status + '</p>'
-                    bot.update_status(status)
-                    item = TwitterDB()
-                    item.reddit_id = myid
-                    item.put()
-                else:
-                    continue
-        self.response.out.write("Done!\n" + tweets)
+				if len(res) == 0 and num_comments > 5:
+					tweets += '<p>' + status + '</p>'
+					bot.update_status(status)
+					item = TwitterDB()
+					item.reddit_id = myid
+					item.put()
+				else:
+					continue
+		self.response.out.write("Done!\n" + tweets)
                 
                 
 application = webapp.WSGIApplication([('/bots/twitterbot', TwitterBot)],
                                      debug=True)
 
 def main():
-    run_wsgi_app(application)
+	run_wsgi_app(application)
 
 if __name__ == "__main__":
-    main()
+	main()
                 
         
         
